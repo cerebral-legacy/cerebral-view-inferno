6319d1cc16f7f2069f051534b45e1fda4f9d6dc1
Christian Alfoni <christianalfoni@gmail.com>
2016-07-08 23:27:46 +0200

    refactor(linting): trying to add linter

--- src/container.js
+++ src/container.js
@@ -1,21 +1,19 @@
-var Inferno = require('inferno');
-var InfernoDOM = require('inferno-dom');
-var createElement = require('inferno-create-element');
-var Component = require('inferno-component');
-var Overlay = require('./Overlay');
+var InfernoDOM = require('inferno-dom')
+var createElement = require('inferno-create-element')
+var Component = require('inferno-component')
 
 class Container extends Component {
-  constructor(props) {
-    super(props);
-    this.componentsMap = {};
-    this.overlays = {};
-    this.overlaysContainer = null;
-    this.registerComponent = this.registerComponent.bind(this);
-    this.unregisterComponent = this.unregisterComponent.bind(this);
-    this.updateComponent = this.updateComponent.bind(this);
-    this.onCerebralUpdate = this.onCerebralUpdate.bind(this);
+  constructor (props) {
+    super(props)
+    this.componentsMap = {}
+    this.overlays = {}
+    this.overlaysContainer = null
+    this.registerComponent = this.registerComponent.bind(this)
+    this.unregisterComponent = this.unregisterComponent.bind(this)
+    this.updateComponent = this.updateComponent.bind(this)
+    this.onCerebralUpdate = this.onCerebralUpdate.bind(this)
   }
-  getChildContext() {
+  getChildContext () {
     return {
       cerebral: {
         controller: this.props.controller,
@@ -25,51 +23,51 @@ class Container extends Component {
       }
     }
   }
-  componentWillMount() {
-    var container = this;
-    this.props.controller.on('flush', this.onCerebralUpdate);
+  componentWillMount () {
+    var container = this
+    this.props.controller.on('flush', this.onCerebralUpdate)
 
-    var container = this;
+    var container = this
     window.addEventListener('cerebral.dev.componentMapPath', function (event) {
-      container.updateOverlays(container.componentsMap[event.detail.mapPath]);
+      container.updateOverlays(container.componentsMap[event.detail.mapPath])
     })
   }
   extractComponentName(component) {
-    return component.constructor.displayName.replace('CerebralWrapping_', '');
+    return component.constructor.displayName.replace('CerebralWrapping_', '')
   }
-  onCerebralUpdate(changes) {
-    var componentsMap = this.componentsMap;
+  onCerebralUpdate (changes) {
+    var componentsMap = this.componentsMap
     function traverse(level, currentPath, componentsToRender) {
       Object.keys(level).forEach(function (key) {
-        currentPath.push(key);
-        var stringPath = currentPath.join('.');
+        currentPath.push(key)
+        var stringPath = currentPath.join('.')
         if (componentsMap[stringPath]) {
           componentsToRender = componentsMap[stringPath].reduce(function (componentsToRender, component) {
             if (componentsToRender.indexOf(component) === -1) {
-              return componentsToRender.concat(component);
+              return componentsToRender.concat(component)
             }
-            return componentsToRender;
+            return componentsToRender
           }, componentsToRender)
         }
         if (level[key] !== true) {
-          componentsToRender = traverse(level[key], currentPath, componentsToRender);
+          componentsToRender = traverse(level[key], currentPath, componentsToRender)
         }
-        currentPath.pop();
-      });
-      return componentsToRender;
+        currentPath.pop()
+      })
+      return componentsToRender
     }
-    var start = Date.now();
-    var componentsToRender = traverse(changes, [], []);
+    var start = Date.now()
+    var componentsToRender = traverse(changes, [], [])
     componentsToRender.forEach(function (component) {
-      component._update();
+      component._update()
     })
-    var end = Date.now();
+    var end = Date.now()
 
     if (process.env.NODE_ENV !== 'production' && componentsToRender.length) {
-      var container = this;
+      var container = this
       var devtoolsComponentsMap = Object.keys(componentsMap).reduce(function (devtoolsComponentsMap, key) {
-        devtoolsComponentsMap[key] = componentsMap[key].map(container.extractComponentName);
-        return devtoolsComponentsMap;
+        devtoolsComponentsMap[key] = componentsMap[key].map(container.extractComponentName)
+        return devtoolsComponentsMap
       }, {})
       var event = new CustomEvent('cerebral.dev.components', {
         detail: {
@@ -87,18 +85,18 @@ class Container extends Component {
   }
   renderOverlays() {
     if (this.isShowingOverlays) {
-      return;
+      return
     }
-    this.isShowingOverlays = true;
-    var overlaysContainer = this.overlaysContainer;
+    this.isShowingOverlays = true
+    var overlaysContainer = this.overlaysContainer
     if (!overlaysContainer) {
-      overlaysContainer = document.createElement('div');
-      overlaysContainer.style.position = 'absolute';
-      overlaysContainer.style.left = '0px';
-      overlaysContainer.style.top = '0px';
-      document.body.appendChild(overlaysContainer);
+      overlaysContainer = document.createElement('div')
+      overlaysContainer.style.position = 'absolute'
+      overlaysContainer.style.left = '0px'
+      overlaysContainer.style.top = '0px'
+      document.body.appendChild(overlaysContainer)
     }
-    var overlays = this.overlays;
+    var overlays = this.overlays
     InfernoDOM.render(
       createElement('div', {
         id: 'cerebral_render_overlay',
@@ -110,59 +108,59 @@ class Container extends Component {
       }, overlays.map(function (overlay, index) {
         return createElement(Overlay, {key: index, overlay: overlay, index: index})
       })
-    ), overlaysContainer);
+    ), overlaysContainer)
 
-    var container = this;
+    var container = this
     setTimeout(function () {
-      document.querySelector('#cerebral_render_overlay').style.opacity = 0.3;
-    }, 10);
+      document.querySelector('#cerebral_render_overlay').style.opacity = 0.3
+    }, 10)
     setTimeout(function () {
-      document.querySelector('#cerebral_render_overlay').style.opacity = 0;
+      document.querySelector('#cerebral_render_overlay').style.opacity = 0
       setTimeout(function () {
-        InfernoDOM.unmountComponentAtNode(overlaysContainer);
-        container.isShowingOverlays = false;
-      }, 500);
-    }, 2000);
+        InfernoDOM.unmountComponentAtNode(overlaysContainer)
+        container.isShowingOverlays = false
+      }, 500)
+    }, 2000)
   }
-  updateOverlays(componentsToRender) {
+  updateOverlays (componentsToRender) {
 
-    var container = this;
+    var container = this
 
     this.overlays = componentsToRender.map(function (component) {
       return {
         bounds: InfernoDOM.findDOMNode(component).getBoundingClientRect(),
         offset: document.body.scrollTop
-      };
-    });
+      }
+    })
 
-    this.renderOverlays();
+    this.renderOverlays()
 
   }
-  registerComponent(comp, deps) {
+  registerComponent (comp, deps) {
     this.componentsMap = Object.keys(deps).reduce(function (componentsMap, key) {
-      componentsMap[key] = componentsMap[key] ? componentsMap[key].concat(comp) : [comp];
-      return componentsMap;
-    }, this.componentsMap);
+      componentsMap[key] = componentsMap[key] ? componentsMap[key].concat(comp) : [comp]
+      return componentsMap
+    }, this.componentsMap)
   }
-  updateComponent(comp, deps) {
-    this.unregisterComponent(comp);
-    this.registerComponent(comp, deps);
-    comp._update();
+  updateComponent (comp, deps) {
+    this.unregisterComponent(comp)
+    this.registerComponent(comp, deps)
+    comp._update()
   }
   unregisterComponent(comp) {
-    var componentsMap = this.componentsMap;
+    var componentsMap = this.componentsMap
     Object.keys(componentsMap).forEach(function (key) {
       if (componentsMap[key].indexOf(comp) >= 0) {
-        componentsMap[key].splice(componentsMap[key].indexOf(comp), 1);
+        componentsMap[key].splice(componentsMap[key].indexOf(comp), 1)
       }
       if (componentsMap[key].length === 0) {
-        delete componentsMap[key];
+        delete componentsMap[key]
       }
-    });
+    })
   }
   render() {
-    return createElement('div', this.props, this.props.children);
+    return createElement('div', this.props, this.props.children)
   }
 }
 
-module.exports = Container;
+module.exports = Container
--- src/hoc.js
+++ src/hoc.js
@@ -1,24 +1,24 @@
 var Inferno = require('inferno')
-var createElement = require('inferno-create-element');
-var InfernoComponent = require('inferno-component');
+var createElement = require('inferno-create-element')
+var InfernoComponent = require('inferno-component')
 
 function extractDeps(deps, allDeps) {
   return Object.keys(deps).reduce(function (depsMap, key) {
     if (deps[key].getDepsMap) {
-      return extractDeps(deps[key].getDepsMap(), allDeps);
+      return extractDeps(deps[key].getDepsMap(), allDeps)
     } else {
-      var depsKey = Array.isArray(deps[key]) ? deps[key].join('.') : deps[key];
-      depsMap[depsKey] = true;
+      var depsKey = Array.isArray(deps[key]) ? deps[key].join('.') : deps[key]
+      depsMap[depsKey] = true
     }
-    return depsMap;
-  }, allDeps);
+    return depsMap
+  }, allDeps)
 }
 
 function functionName(fun) {
-  var ret = fun.toString();
-  ret = ret.substr('function '.length);
-  ret = ret.substr(0, ret.indexOf('('));
-  return ret;
+  var ret = fun.toString()
+  ret = ret.substr('function '.length)
+  ret = ret.substr(0, ret.indexOf('('))
+  return ret
 }
 
 module.exports = function (Component, paths) {
@@ -32,23 +32,23 @@ module.exports = function (Component, paths) {
       if (!Object.keys(statePaths).length) {
         return
       }
-      this.context.cerebral.registerComponent(this, this.getDepsMap(this.props));
+      this.context.cerebral.registerComponent(this, this.getDepsMap(this.props))
     }
     componentWillUnmount() {
       this._isUmounting = true
-      this.context.cerebral.unregisterComponent(this);
+      this.context.cerebral.unregisterComponent(this)
     }
     shouldComponentUpdate() {
-      return false;
+      return false
     }
     componentWillReceiveProps(nextProps) {
-      var hasChange = false;
-      var oldPropKeys = Object.keys(this.props);
-      var newPropKeys = Object.keys(nextProps);
+      var hasChange = false
+      var oldPropKeys = Object.keys(this.props)
+      var newPropKeys = Object.keys(nextProps)
       if (oldPropKeys.length !== newPropKeys.length) {
-        hasChange = true;
+        hasChange = true
       } else {
-        for (var i = 0; i < newPropKeys.length; i++) {
+        for (var i = 0 i < newPropKeys.length i++) {
           if (this.props[newPropKeys[i]] !== nextProps[newPropKeys[i]]) {
             hasChange = true
             break
@@ -59,16 +59,16 @@ module.exports = function (Component, paths) {
       if (typeof paths === 'function') {
         this.context.cerebral.updateComponent(this, this.getDepsMap(nextProps))
       } else {
-        hasChange && this._update();
+        hasChange && this._update()
       }
     }
     getProps() {
-      var controller = this.context.cerebral.controller;
+      var controller = this.context.cerebral.controller
       var props = this.props || {}
       var paths = this.getStatePaths ? this.getStatePaths(this.props) : {}
 
       var propsToPass = Object.keys(paths || {}).reduce(function (props, key) {
-        props[key] = paths[key].getDepsMap ? paths[key].get(controller.get()) : controller.get(paths[key]);
+        props[key] = paths[key].getDepsMap ? paths[key].get(controller.get()) : controller.get(paths[key])
         return props
       }, {})
 
@@ -83,30 +83,28 @@ module.exports = function (Component, paths) {
       return propsToPass
     }
     _update(showOverlay) {
-      console.log('I should force update');
       if (this._isUmounting) {
-        return;
+        return
       }
       if (showOverlay && !this.showingOverlay) {
-        this.showOverlay = true;
+        this.showOverlay = true
       }
-      console.log('force updating!');
-      this.forceUpdate();
+      this.forceUpdate()
     }
     getPropsWithModules(props) {
       return Object.keys(props).reduce(function (propsWithModules, key) {
         propsWithModules[key] = props[key]
         return propsWithModules
-      }, {modules: this.modules});
+      }, {modules: this.modules})
     }
     getDepsMap(props) {
       if (!paths) {
         return {}
       }
-      var propsWithModules = this.getPropsWithModules(props);
-      var deps = typeof paths === 'function' ? paths(propsWithModules) : paths;
+      var propsWithModules = this.getPropsWithModules(props)
+      var deps = typeof paths === 'function' ? paths(propsWithModules) : paths
 
-      return extractDeps(deps, {});
+      return extractDeps(deps, {})
     }
     getStatePaths(props) {
       if (!paths) {
@@ -119,12 +117,11 @@ module.exports = function (Component, paths) {
       return typeof paths === 'function' ? paths(propsWithModules) : paths
     }
     render() {
-      console.log('Rerendering!');
       return createElement(Component, this.getProps())
     }
   }
 
-  CerebralComponent.displayName = 'CerebralWrapping_' + functionName(Component);
+  CerebralComponent.displayName = 'CerebralWrapping_' + functionName(Component)
 
-  return CerebralComponent;
-};
+  return CerebralComponent
+}
