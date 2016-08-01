68bc39e0c2a78bf43d35e8f131dfad9588a95f8f
Christian Alfoni <christianalfoni@gmail.com>
2016-08-01 19:03:44 +0200

    feat(testing): add return component for testing

--- src/connect.js
+++ src/connect.js
@@ -8,9 +8,9 @@ module.exports = function connect (paths, signals, component) {
 
   if (!component) {
     return function (component) {
-      return HOC(paths, signals, component)
+      return process.env.NODE_ENV === 'test' ? component : HOC(paths, signals, component)
     }
   } else {
-    return HOC(paths, signals, component)
+    return process.env.NODE_ENV === 'test' ? component : HOC(paths, signals, component)
   }
 }
